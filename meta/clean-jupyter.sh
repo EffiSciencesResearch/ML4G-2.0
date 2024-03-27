f599c67b11070403fc86e46f1f3978e1956ac61f
ddorn <diego.dorn@free.fr>
2024-03-27 19:54:08 +0100

    give up on the colab warning

--- meta/clean-jupyter.sh
+++ meta/clean-jupyter.sh
@@ -8,7 +8,7 @@ for file in "$@"; do
         '
         (.cells[] | select(has("outputs")) | .outputs) = []
         | (.cells[] | select(has("execution_count")) | .execution_count) = null
-        | .metadata = {"language_info": {"name":"python", "pygments_lexer": "ipython3"}, "kernelspec": {"name": "python3", "display_name": "Python 3"}}
+        | .metadata = {"language_info": {"name":"python", "pygments_lexer": "ipython3"}}
         | .cells[].metadata = {}
         ' "$file")
     echo "$out" > "$file"
