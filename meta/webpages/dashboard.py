6b20f7e0503db7d0828f502963789326e0ab659d
ddorn <diego.dorn@free.fr>
2025-03-06 20:50:14 +0100

    feat(web): Add nice names for pages
    * Refactor main Streamlit application in `meta/web.py` to use a navigation system with a registrar for dynamically managing pages and navigation buttons.
    * Modularized existing page-specific logic by relocating specific pages such as `career_docs`, `edit_camp`, and `ta_assignment` to a new `meta/webpages` directory, allowing for improved organization and scalability of the application.
    * Introduce a new main `dashboard` page source under `meta/webpages/dashboard.py`, which encompasses text and login functionality previously residing in `meta/web.py`, maintaining full functionality while adopting a modular approach.
    * Updated imports to reflect the new directory structure and removed unnecessary imports such as `dotenv` in `meta/web.py` to streamline the application structure.
    * Create a scheme to add newly discovered Python scripts in the `webpages` directory as new pages in the navigation when not explicitly specified in `pages`.

---
+++ meta/webpages/dashboard.py
@@ -0,0 +1,72 @@
+import os
+
+import streamlit as st
+import dotenv
+
+from camp_utils import Camp
+from streamlit_utils import State
+
+dotenv.load_dotenv()
+
+
+st.title("Welcome to the ML4G tools portal!")
+
+st.markdown(
+    """
+This page is and will grow into a collection of handy tools to
+help run the ML4G bootcamps.
+
+We aim to make those tools self-explanatory, so that they
+require *no* external documentation, but if you find
+yourself confused or things are broken, please reach out
+to Diego or the ML4G team on Slack.
+
+This page lets you select which camp to use for all the tools and create a new one.
+
+Enjoy! :rocket:
+"""
+)
+
+
+state = State()
+
+if state.current_camp:
+    st.write(f"## Currently editing camp `{state.current_camp.name}`")
+else:
+    st.write("## Start by selecting a camp")
+
+
+# Now two options: select a new camp or create a new one
+col_select_camp, col_new_camp = st.columns(2)
+
+
+with col_select_camp:
+    st.header("Select a camp")
+
+    with st.container(border=True):
+        state.login_form()
+
+
+with col_new_camp:
+    st.header("Create a new camp")
+
+    with st.form("create_camp"):
+        name = st.text_input("Name")
+        date = st.date_input("Date")
+
+        disabled = "ENABLE_CREATE_CAMP" not in os.environ
+        letsgooo = st.form_submit_button("Create camp", disabled=disabled)
+        if disabled:
+            st.warning("It's not yet possible to create new camp from the public portal.")
+
+    if letsgooo:
+        camp = Camp.new(name=name, date=date.strftime("%Y-%m-%d"))
+        camp.save_to_disk()
+
+        st.warning(
+            "### Please write down the password for this camp. It will not be shown again.\n\n"
+            f"### Password: `{camp.password}`"
+        )
+        st.write("You can continue configuring the camp in **Edit Camp** on the left.")
+
+        state.login(camp.name, camp.password)
